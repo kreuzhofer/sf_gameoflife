b2729edbd6bddcf1177b2e21c6fbdbf0f3f6c1d0
Peter Kirchner <peter.kirchner@microsoft.com>
2016-06-08 14:46:59 +0200

    fixed bug

--- src/SFGameOfLife/OrchestrationActor/OrchestrationActor.cs
+++ src/SFGameOfLife/OrchestrationActor/OrchestrationActor.cs
@@ -25,7 +25,7 @@ namespace OrchestrationActor
     internal class OrchestrationActor : Actor, IOrchestrationActor
     {
 
-        private List<Cell> cells;
+        private List<Cell> cells = new List<Cell>;
 
         public Task BigBang()
         {
